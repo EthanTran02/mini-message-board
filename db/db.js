21c0cd26585a97c017bda9e6a0c1b0cf04100c90
Ethan Tran <khoitranec@gmail.com>
2025-07-05 17:38:02 +0700

    Fix confic for local db

--- db/db.js
+++ db/db.js
@@ -11,7 +11,6 @@ const pool = connectionString
       database: process.env.DB_NAME,
       password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT,
-      ssl: { rejectUnauthorized: false },
     });
 
 module.exports = pool;
